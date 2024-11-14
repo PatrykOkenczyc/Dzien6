ef33fad94657256717c72bc80c469c6a940ce234
Patryk Okenczyc <Xomel@outlook.com>
2024-11-14 09:28:23 +0100

    Add project files.

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connection;
+            SqlCommand command;
+            SqlDataReader sqlDataReader;
+
+            string connectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=A_Zawodnicy;Integrated Security=True";
+
+            connection = new SqlConnection(connectionString);
+
+            command = new SqlCommand("select * from zawodnicy", connection);
+
+            connection.Open();
+            sqlDataReader = command.ExecuteReader();
+
+            while (sqlDataReader.Read())
+            {
+
+                string wynik = (string)sqlDataReader.GetValue(2)+" "+ (string)sqlDataReader.GetValue(3);
+                Console.WriteLine(wynik);
+                
+            }
+            connection.Close();
+            Console.ReadKey();
+
+        }
+    }
+}
